6b9273cba88567b6b0cef9af9fe0c9c8257cbccf
Bacon <bacon.antife@gmail.com>
2023-02-09 13:19:57 -0300

    Geometry of celestial sphere cleaned up

--- js/webgl2/implementations/dynamic-geometries.js
+++ js/webgl2/implementations/dynamic-geometries.js
@@ -29,35 +29,8 @@ export const horizon = ({ dip, radius }) => {
 const base_mag = -1.45;
 const magToRad = (mag, maxRad) => Math.pow(Math.pow(2.512, base_mag - mag), 0.25)*maxRad;
 
-export const celestialSphere = (stars, maxRad) => {
-	const nVertices = 5;
+export const celestialSphere = () => {
 	const attr = [];
 	const element = [];
-	let vCount = 0;
-	const addStar = ({ ra, dec, vmag }) => {
-		const rad = magToRad(vmag, maxRad);
-		const raAngle = ra/12*Math.PI;
-		const decAngle = dec/180*Math.PI;
-		const transform = new Mat4();
-		transform.rotateX(decAngle);
-		transform.rotateZ(-raAngle);
-		const center = new Vec3([ 0, 1, 0 ]).apply(transform);
-		const color = [ 1, 1, 1 ];
-		attr.push(
-			...center,
-			...color,
-		);
-		for (let i=0; i<nVertices; ++i) {
-			const angle = i/nVertices*Math.PI*2;
-			const x = Math.sin(angle)*rad;
-			const z = Math.cos(angle)*rad;
-			const vec = new Vec3([ x, 1, z ]).apply(transform).normalize();
-			attr.push(...vec, ...color);
-			const triangle = [ 0, 1 + i, 1 + (i + 1)%nVertices ].map(i => i + vCount);
-			element.push(...triangle);
-		}
-		vCount += nVertices + 1;
-	};
-	stars.forEach(addStar);
-	return new Geometry({ attr, element, layout: [ 3, 3 ] });
+	return new Geometry({ attr, element, layout: [ 3 ] });
 };
