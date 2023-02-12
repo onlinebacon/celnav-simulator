f9c0639dbcc506dc95a6db99228e1e8e536bbea9
Bacon <bacon.antife@gmail.com>
2023-02-12 17:18:55 -0300

    Using split programs for atmosphere in sextant view

--- js/webgl2/implementations/items/atmosphere.js
+++ js/webgl2/implementations/items/atmosphere.js
@@ -1,10 +1,21 @@
-import { atmosphere as prog } from '../programs.js';
 import { atmosphere as geometry } from '../static-geometries.js';
+import {
+    atmosphere as prog,
+    atmosphereL as progL,
+    atmosphereR as progR,
+} from '../programs.js';
 import { Mat4 } from '../../core/gl-math.js';
+
+const progMap = {
+    C: prog,
+    L: progL,
+    R: progR,
+};
 
 const transform = new Mat4();
 
 export const draw = (ctx, camera, side) => {
+    const prog = progMap[side];
     ctx.useProgram(prog);
     prog.setMat4Uniform('transform', transform);
     prog.setMat4Uniform('projection', camera.projection);
